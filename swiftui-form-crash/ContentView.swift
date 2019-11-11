d27ba3baf3f80e1a23d63ffdbb7bf7594e939cfb
Majid Jabrayilov <cmecid@gmail.com>
2019-11-11 21:20:48 +0400

    Initial Commit

---
+++ swiftui-form-crash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-form-crash
+//
+//  Created by Majid Jabrayilov on 11/11/19.
+//  Copyright © 2019 Majid Jabrayilov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
